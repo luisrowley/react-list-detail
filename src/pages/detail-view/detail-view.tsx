d653829070e6e12f44929224a8d200d508d43597
luisrowley <louisdebenito@gmail.com>
2022-07-04 21:18:35 +0200

    added properties with icons

--- src/pages/detail-view/detail-view.tsx
+++ src/pages/detail-view/detail-view.tsx
@@ -1,38 +1,72 @@
-import { Avatar, Button, Grid } from '@mui/material';
+import { Avatar, Button, CircularProgress, Grid, List, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import './detail-view.css';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import HeightIcon from '@mui/icons-material/Height';
+import ScaleIcon from '@mui/icons-material/Scale';
+import { deepPurple } from '@mui/material/colors';
 
 const DetailView: React.FC<any> = (props: any) => {
 
   const { state } : any = useLocation();
   const [character, setCharacter]:any = React.useState({});
+  const [loading, setLoading]:any = React.useState(false);
 
   useEffect(() => {
     setCharacter(state.row)
   }, [state])
 
-  const setCharacterByName = (charname: string) => {
-    setCharacter(props.charData.find((character: any) => character.name === charname));
+  const setCharacterByName = async (charname: string) => {
+    setLoading(true);
+    let character = await props.charData.find((character: any) => character.name === charname);
+    if(character) {
+      setLoading(false);
+      setCharacter(character);
+    }
   }
 
   return (
     <main className="profile-container">
       <Grid container spacing={2} className="grid-main">
         <Grid item className="grid-section" xs={4}>
+          { loading ? <CircularProgress/> : 
           <div>
           <Avatar 
             sx={{ width: 256, height: 256 }}
             alt={character.name} src={character.thumbnail} />
             {character.name}
           </div>
+          }
         </Grid>
-        <Grid container className="grid-section" xs={8}>
+        <Grid item container className="grid-section" xs={8}>
           <Grid item xs={12}>
-            <div>xs=34</div>
-            <div>xs=34</div>
-            <div>xs=34</div>
-            <div>xs=34</div>
+          <List sx={{ float: "right", width: "750px" }}>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar sx={{ bgcolor: deepPurple[100] }}>
+                  <FaceRetouchingNaturalIcon color="secondary" />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Age" secondary={character.age} />
+            </ListItem>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar sx={{ bgcolor: deepPurple[100] }}>
+                  <ScaleIcon color="secondary" />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Weight" secondary={character.weight} />
+            </ListItem>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar sx={{ bgcolor: deepPurple[100] }}>
+                  <HeightIcon color="secondary" />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Height" secondary={character.weight} />
+            </ListItem>
+          </List>
           </Grid>
           <Grid item xs={12}>
             {character.friends && character.friends.length > 0 ?
